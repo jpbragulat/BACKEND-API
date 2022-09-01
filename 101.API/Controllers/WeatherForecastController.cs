0131fe839d04326d48e3692a9fca988eb932d7f1
jpbragulat <107953098+jpbragulat@users.noreply.github.com>
2022-09-01 13:43:58 -0300

    Add files via upload

---
+++ 101.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace _101.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _logger.Log(LogLevel.Information, "pase x constructor");
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.Log(LogLevel.Information, "pase x aca");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ 101.API/WeatherForecast.cs
@@ -0,0 +1,19 @@
+namespace _101.API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+
+    }
+
+    public class Prueba:WeatherForecast
+    {
+         
+    }
+}
